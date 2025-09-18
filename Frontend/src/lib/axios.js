8aba9ce225d6d3e86155ba92e0b72b76cac50ae7
sachin_sparky <sachin2322006@gmail.com>
2025-09-18 16:53:56 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://ssr-talk.onrender.com",
     withCredentials: true,
 })
